35cdc0bfe38fe71344c69931a4c114578c9d73c9
vosaul <v986963@gmail.com>
2021-03-22 09:49:25 +0200

    testimonials updated

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -6,8 +6,11 @@ const Testimonials = () => {
     return (
       <div className="testimonial_card" key={key}>
         <img src={tm.icon} alt="{tm.name}" />
-        <q>{tm.quote} </q>
-        <p>{tm.name}</p>
+        <p className="quote">
+          <img src="/svg/QMark.svg" alt="Quote Mark" />
+          {tm.quote}{" "}
+        </p>
+        <h4>{tm.name}</h4>
         <p>{tm.position}</p>
       </div>
     );
@@ -15,7 +18,8 @@ const Testimonials = () => {
   return (
     <section id="testimonials_section" className="testimonials wrapper">
       <div className="testimonials_head">
-        <h2>{Testimonis.title}</h2>
+        <h2><img src="/svg/QuoteMark.svg" alt="Quote Mark" />
+          {Testimonis.title}</h2>
         <p>{Testimonis.subtitle}</p>
       </div>
       <div className="testimonials_list">{Items}</div>

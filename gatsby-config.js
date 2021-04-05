41266cd5639f7db9ba6a153aa695cdbd70cf59f3
Volodymyr Sauliak <3429638@gmail.com>
2021-04-05 12:02:17 +0300

    favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,13 +42,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `GypherX website`,
+        short_name: `GypherX`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/android-chrome-192x192.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

2b55bdd9e9dc5fc3bfe981076292c633f131b904
Volodymyr Sauliak <v986963@gmail.com>
2024-02-03 16:29:47 +0200

    bbb

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,74 +14,38 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   });
 };
 
-exports.createPages = async ({ actions, graphql }) => {
+exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
   const results = await graphql(`
   query allMdxPages {
-    features: allFile(filter: {sourceInstanceName: {eq: "features"}}) {
-      edges {
-        node {
-          childrenMdx {
-            frontmatter {
-              description
-              image
-              slug
-              title
-            }
-
-            body
-          }
-
-          sourceInstanceName
-          name
-        }
-      }
-    }
-
-    posts: allFile(filter: {sourceInstanceName: {eq: "posts"}}) {
-      edges {
-        node {
-          childrenMdx {
-            frontmatter {
-              description
-              image
-              slug
-              title
-            }
-
-            body
-          }
-
-          sourceInstanceName
-        }
-      }
-    }
-
     allMdx(filter: {frontmatter: {type: {eq: "feature"}}}) {
       edges {
         node {
           frontmatter {
-            title
-            image
-            slug
+            permalink
           }
+
           id
-          body
-        }
+          internal {
+            contentFilePath
+          }        }
       }
     }
   }
 
 `)
+if (results.errors) {
+  reporter.panicOnBuild('Error loading MDX result', result.errors)
+}
+
+const postTemplate = path.resolve(`./src/templates/detailsPage.js`)
+
 results.data.allMdx.edges.forEach(item => {
-  console.log(item,"gggggggggg");
   createPage({
-        path: `/features/${item.node.frontmatter.slug}/`,
-        component: require.resolve("./src/templates/detailsPage.js"),
+        path: `/features/${item.node.frontmatter.permalink}/`,
+        component: item.node.internal.contentFilePath,
         context: {
-          title: item.node.frontmatter.title,
-          image: item.node.frontmatter.image,
-          body: item.node.body,
+          id: item.node.id,
         },
       })
     }
--- src/templates/detailsPage.js
+++ src/templates/detailsPage.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import MDXRenderer from 'gatsby-plugin-mdx';
+import React from "react"
+import { graphql } from "gatsby"
+import { MDXProvider } from "@mdx-js/react"
+import { Link } from "gatsby"
 import Layout from '~components/Layout'
+
+const shortcodes = { Link } // Provide common components here
 
 const header = {
   headerClasses: "site-header site-header--menu-end dark-header site-header--sticky",
@@ -8,16 +12,32 @@ const header = {
   darkLogo:false,
 }
 
-export default function Service({
-  pageContext,}) {
+function PostTemplate({ data: { mdx }, children }) {
+  console.log(mdx,children);
   return (
     <Layout innerPage={true} headerConfig={header}>
       <div class="post_wrapper">
-          <h1>{pageContext.title}</h1>
-          <img src={pageContext.image} alt={pageContext.title} />
+          <h1>{mdx.frontmatter.title}</h1>
+          <img src={mdx.frontmatter.image} alt={mdx.frontmatter.title} />
           <hr/>
-          <MDXRenderer>{pageContext.body}</MDXRenderer> 
+          <MDXProvider components={shortcodes}>
+            {children}
+            <p>Lorem ipsum dolor</p>
+          </MDXProvider>
       </div>
     </Layout>
   );
 }
+
+export const pageQuery = graphql`
+  query PostTemplate($id: String!) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        image
+      }
+    }
+  }
+
+`
+export default PostTemplate
